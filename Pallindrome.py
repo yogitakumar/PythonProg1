9177f4b29a23130a222ca62fc9204ca01b209f2a
Yogita Kumar <61360028+yogitakumar@users.noreply.github.com>
2020-12-14 05:08:51 +0000

    Add files via upload

---
+++ Pallindrome.py
@@ -0,0 +1,7 @@
+n = str(input("Enter string : "))
+n_in_reverse = n[::-1]
+print(n_in_reverse)
+if(n == n_in_reverse) : 
+    print("Given string is pallindrome")
+else : 
+    print("Given dtring is not pallindrome")
